daf13cd25b9c91dd88c3d312ccf6f2a71d0bfe36
mahiuddinhabib <mh.habib137@gmail.com>
2022-10-02 11:46:27 +0600

    next prev buttons are working

--- src/Scheduler/Calender.js
+++ src/Scheduler/Calender.js
@@ -1,39 +1,61 @@
 import React, { useReducer, useState } from 'react';
 import Day from './Day';
 
-function reducer(day, action){
+function reducer(day, action) {
 
 }
 const Calender = () => {
     const calender = [];
-    const [date, setDate] = useState(new Date().getDate());
-    const [month, setMonth] = useState(new Date().getMonth());
-    const [year, setYear] = useState(new Date().getFullYear());
+    const [sDate, setSdate] = useState(new Date().getDate());
+    const [sMonth, setSmonth] = useState(new Date().getMonth());
+    const [sYear, setSyear] = useState(new Date().getFullYear());
     const [calendr, setCalendr] = useReducer(reducer, calender)
-    
+
     const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-    const totalDays = 32 - new Date(year, month, 32).getDate();
+    const months = ['january', 'february', 'march', 'april', 'may', 'jun', 'july', 'august', 'september', 'october', 'november', 'december']
+    const totalDays = 32 - new Date(sYear, sMonth, 32).getDate();
     for (let i = 0; i < totalDays; i++) {
-        calender.push({date: i});
+        calender.push({ date: i });
 
     }
     // console.log(calender);
-    
+
+    function next() {
+        const year = (sMonth === 11) ? sYear + 1 : sYear;
+        const month = (sMonth + 1) % 12;
+        setSyear(year);
+        setSmonth(month);
+    }
+
+    function previous() {
+        const year = (sMonth === 0) ? sYear - 1 : sYear;
+        const month = (sMonth === 0) ? 11 : sMonth - 1;
+        setSyear(year);
+        setSmonth(month);
+    }
+
     return (
-        <div className='col-span-3 grid grid-cols-7'>
-            {
-                days.map((day, index)=><h3 
-                className='p-4 m-1 bg-orange-300'
-                key={index}>{day}</h3>)
-            }
-            {
-                calender.map(day=><Day
-                key={day.date}
-                day={day}
-                year={year}
-                month={month}
-                ></Day>)
-            }
+        <div className='col-span-3'>
+            <div>
+                <button onClick={previous}>previous</button>
+                <span className='uppercase'> {months[sMonth]}, {sYear} </span>
+                <button onClick={next}>next</button>
+            </div>
+            <div className='grid grid-cols-7'>
+                {
+                    days.map((day, index) => <h3
+                        className='p-4 m-1 bg-orange-300'
+                        key={index}>{day}</h3>)
+                }
+                {
+                    calender.map(day => <Day
+                        key={day.date}
+                        day={day}
+                        sYear={sYear}
+                        sMonth={sMonth}
+                    ></Day>)
+                }
+            </div>
         </div>
     );
 };
--- src/Scheduler/Day.js
+++ src/Scheduler/Day.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const Day = ({day, year, month}) => {
-     const {date} = day;
-    //  const startDay = `col-start-${(new Date(year, month)).getDay()+1}`;
-    // console.log(startDay);
-    return (
-        <div className={`px-4 py-10 m-1 bg-lime-300 ${date===0?`col-start-${(new Date(year, month)).getDay()+1}`:''}`}>
-            <h1>{date+1}</h1>
-        </div>
-    );
+const Day = ({ day, sYear, sMonth }) => {
+    const { date } = day;
+    const toDay = new Date().getDate()-1;
+    const toMonth = new Date().getMonth();
+    const toYear = new Date().getFullYear();
+    const startDay = new Date(sYear, sMonth).getDay();
+
+return (
+    <div 
+    className={`px-2 py-6 m-1 bg-lime-300`} 
+    style={{gridColumnStart: date===0? startDay+1:null, backgroundColor: date===toDay && sMonth===toMonth && sYear===toYear?'red':''}}>
+        <h1>{date + 1}</h1>
+    </div>
+);
 };
 
 export default Day;
